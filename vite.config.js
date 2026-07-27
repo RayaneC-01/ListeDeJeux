a52569a8e6ed9ab2da4643cd76a7848a5f8ce56a
RayaneC-01 <rchaabane20@gmail.com>
2026-07-27 15:37:54 +0200

    chore: configuration de la PWA et du mode hors-ligne

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
+      },
+      manifest: {
+        name: 'Liste De Jeux',
+        short_name: 'Jeux',
+        description: 'Ma super collection de jeux web hors ligne',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=192&h=192&fit=crop',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=512&h=512&fit=crop',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpJtIsv4PMb68gVExy76J62a7xyGX9-yG9Gk384gJgyQ&s=10",
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/ListeDeJeux/',
+  build: {
+    cssMinify: 'esbuild'
+  }
 })
